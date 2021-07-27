d9fbb10845287a2ff880e19cf522c454310b459e
siou <siou10377@gmail.com>
2021-07-27 16:07:46 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+
 app.get('/', (req, res) => {
-  res.send('This is my restaurant list built with Express')
+  res.render('index')
 })
 
 app.listen(port, () => {
