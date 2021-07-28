a9d00f0b561f0420f01c8576898db825bb529752
siou <siou10377@gmail.com>
2021-07-28 14:00:10 +0800

    feat: add bootstrap

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
