5acfb1b5061fb9e123bad760d2010f4ffdd0d759
siou <siou10377@gmail.com>
2021-07-28 23:14:38 +0800

    feat: add json and modify index page

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const restaurantLists = require('./restaurant.json')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantLists.results })
 })
 
 app.listen(port, () => {
