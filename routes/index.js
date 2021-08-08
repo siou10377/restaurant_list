0ae4ed3f8cea7ca2b48f16ed972afe61c951f2f7
siou <siou10377@gmail.com>
2021-08-08 23:13:31 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const search = require('./modules/search')
 const crud = require('./modules/crud')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/search', search)
 router.use('/restaurants', crud)
+router.use('/users', users)
 
 module.exports = router
