706a6f722b7c7533aeacf19b76da108d2408d958
siou <siou10377@gmail.com>
2021-08-08 21:44:17 +0800

    feat: add update restaurant function

--- routes/modules/crud.js
+++ routes/modules/crud.js
@@ -24,6 +24,32 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// 設定 edit 路由
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurants => res.render('edit', { restaurants }))
+    .catch(error => console.log(error))
+})
 
+router.put('/:id/', (req, res) => {
+  const id = req.params.id
+  const { name, category, image, location, phone, google_map, rating, description } = req.body
+  return Restaurant.findById(id)
+    .then((restaurants) => {
+      restaurants.name = name
+      restaurants.phone = phone
+      restaurants.category = category
+      restaurants.rating = rating
+      restaurants.location = location
+      restaurants.image = image
+      restaurants.google_map = google_map
+      restaurants.description = description
+      return restaurants.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
 
 module.exports = router
