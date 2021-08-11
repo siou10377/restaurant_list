14d89eab7cacfd4088a745f1af7e213c0dcf1f30
siou <siou10377@gmail.com>
2021-08-11 18:29:24 +0800

    feat: add relations between User and Todo models

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.static('public'))
-app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 usePassport(app)
 app.use((req, res, next) => {
@@ -22,11 +29,6 @@ app.use((req, res, next) => {
   next()
 })
 app.use(routers)
-app.use(session({
-  secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
 
 
 app.listen(port, () => {
--- routes/modules/crud.js
+++ routes/modules/crud.js
@@ -9,16 +9,18 @@ router.get('/new', (req, res) => {
 })
 
 router.post("/new", (req, res) => {
-  const newRestaurant = req.body
-  Restaurant.create(newRestaurant)
+  const userId = req.user._id
+  const { name, name_en, category, location, phone, description, image, rating, google_map } = req.body
+  Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
     .then(() => res.redirect("/"))
     .catch(error => console.log(error))
 })
 
 // 設定 show 頁面路由
 router.get('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurants => res.render('show', { restaurants }))
     .catch(error => console.log(error))
@@ -26,17 +28,19 @@ router.get('/:id', (req, res) => {
 
 // 設定 edit 路由
 router.get('/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .lean()
     .then(restaurants => res.render('edit', { restaurants }))
     .catch(error => console.log(error))
 })
 
 router.put('/:id/', (req, res) => {
-  const id = req.params.id
+  const userId = req.user._id
+  const _id = req.params.id
   const { name, category, image, location, phone, google_map, rating, description } = req.body
-  return Restaurant.findById(id)
+  return Restaurant.findOne({ _id, userId })
     .then((restaurants) => {
       restaurants.name = name
       restaurants.phone = phone
@@ -54,8 +58,9 @@ router.put('/:id/', (req, res) => {
 
 // 設定 delete 路由
 router.delete('/:id/', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
+  const userId = req.user._id
+  const _id = req.params.id
+  return Restaurant.findOne({ _id, userId })
     .then(restaurants => {
       return restaurants.remove()
     })
