58e1970a236041f8df9ec2ba3748f9842bea1739
siou <siou10377@gmail.com>
2021-08-12 21:54:24 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser')
 const routers = require('./routes')
 const session = require('express-session')
 const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -23,9 +24,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 app.use(routers)
